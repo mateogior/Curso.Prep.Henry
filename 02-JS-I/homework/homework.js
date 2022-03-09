3e2bb3be1ca9279408c009a5f8b9548f60518411
Mateo <macbook@MacBook-Pro-2.local>
2022-03-09 18:33:10 -0300

    fourth commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -37,7 +37,8 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var suma = x + y;
+  return suma;
 }
 
 function resta(x, y) {
