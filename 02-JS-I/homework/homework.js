d4a991492c772f6ac9b539a5a0ce87a8dad6e032
Mateo <macbook@MacBook-Pro-2.local>
2022-03-09 19:02:56 -0300

    seven commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,8 +75,9 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length === str2.lenght) {return true;
-  } else {return false;}
+  if (str1.length === str2.length) {return true;
+  } else {return false;
+  }
   
 }
 
