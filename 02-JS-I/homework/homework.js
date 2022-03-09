8744403816d3e9a7efff341ac7444e3d6a8b92ec
Mateo <macbook@MacBook-Pro-2.local>
2022-03-09 18:54:56 -0300

    five commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -44,26 +44,31 @@ function suma(x, y) {
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  var resta = x - y;
+  return resta;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  var multiplica = x * y;
+  return multiplica;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  var divide = x / y;
+  return divide;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (x === y) {return true;
+  } else {return false;
+  }
 }
 
 function tienenMismaLongitud(str1, str2) {
