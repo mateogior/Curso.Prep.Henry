0b2b5346d5c45a79ac32d3c53d6c9f400505f8e0
Mateo <macbook@MacBook-Pro-2.local>
2022-03-10 16:20:47 -0300

    prueba final 3

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -205,7 +205,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + nombre + '!';
+  return 'Hola ' + nombre + '!';
 
 }
 
@@ -249,11 +249,11 @@ function esVocal(letra){
     return "Dato incorrecto"
   } if (letra === 'a', letra === 'e', letra === 'i', letra === 'o', letra === 'u'){
     return "Es vocal";
-  }  return "Dato incorrecto";
+  } else {return "Dato incorrecto";
+}
 
   
 }
-
 
 
 
