f64e4c1709ad1e8ed1caba403ba1b628cb17b9b9
Mateo <macbook@MacBook-Pro-2.local>
2022-03-10 14:51:38 -0300

    seven commit v2

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -94,13 +94,15 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num > 50) {return true;
+  } else {return false;
 }
+
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return (x % y);
 }
 
 function esPar(num) {
