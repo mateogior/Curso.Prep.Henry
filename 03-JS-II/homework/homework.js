793247f5f78fab9ed6c493a2f42856282312d88a
Mateo <macbook@MacBook-Pro-2.local>
2022-03-14 15:58:56 -0300

    buenio

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -117,7 +117,7 @@ function fizzBuzz(numero) {
     return "fizz"
   } if ( numero % 5 === 0){
     return "buzz"
-  } if ( numero % 3 === 0 && numero % 5 === 0) {
+  } if ( numero % 15 === 0) {
     return 'fizzbuzz'
   } else {return numero
   }
