7e733035eb6d7d325342f673a8466d5618c5a517
Mateo <macbook@MacBook-Pro-2.local>
2022-03-14 15:33:52 -0300

    aver

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -43,7 +43,14 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
-  
+  if (idioma === aleman){
+    return 'Guten Tag!';
+  } if (idioma === mandarin){
+    return 'Ni Hao!';
+  } if (idioma === ingles){
+    return 'Hello!';
+  } else {return 'Hola!';
+}
 }
 
 function colors(color) {
