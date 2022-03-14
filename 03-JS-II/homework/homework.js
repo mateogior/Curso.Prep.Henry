ac0d881182a6433efaadd2de64c32603ae43ffe0
Mateo <macbook@MacBook-Pro-2.local>
2022-03-14 20:48:20 -0300

    aver

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -153,12 +153,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- if ( numero % numero === 0 && numero % 1 === 0){
+ if ( numero === 2){
    return true
- } else if ( numero === 1 || numero === 0){
-   return false
+ } else if ( numero < 2){
+   return false 
+ } for ( var i === 2 ; i < numero ; i++)
+ if ( numero % i === 0) {
+   return false 
  } else {
-   return false
+   return true
  }
 } 
 
