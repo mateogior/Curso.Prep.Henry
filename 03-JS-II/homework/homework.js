7ac49aaf1c5248927963b2f3ddcfe4f6653600de
Mateo <macbook@MacBook-Pro-2.local>
2022-03-14 15:46:42 -0300

    aver

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -74,7 +74,7 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
- if (numero === 10 || 5){
+ if (numero === 10 || numero === 5){
    return true 
  } else {
    return false
