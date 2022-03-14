7be085821a2547ddd584a9ce0583a7567efa17e6
Mateo <macbook@MacBook-Pro-2.local>
2022-03-14 15:43:10 -0300

    aver

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -62,11 +62,11 @@ function colors(color) {
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
 switch (color) {
-  case 'blue': 'This is blue';
-  case 'red' : 'This is red';
-  case 'green' : 'This is green';
-  case 'orange' : 'This is orange'
-  default : 'Color not found'
+  case 'blue': return 'This is blue';
+  case 'red' : return 'This is red';
+  case 'green' :  return 'This is green';
+  case 'orange' : return 'This is orange'
+  default : return 'Color not found'
 }
 }
 
