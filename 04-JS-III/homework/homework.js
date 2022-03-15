3d564a0b5b44de2835e004675bfae8c332032b89
Mateo <macbook@MacBook-Pro-2.local>
2022-03-15 17:21:41 -0300

    bueno

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -129,6 +129,14 @@ function multiplicarArgumentos() {
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
+var elementosmayores= [];
+for (var i=0; i<arreglo.length; i++){
+  if (arreglo[i]>18){
+    elementosmayores.push(arreglo[i])
+  } return elementosmayores 
+}
+}
+  
 
 }
 
