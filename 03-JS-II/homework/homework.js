05f5bdd8336840f4a2804a3fa1008cf5b65a4c58
Mateo <macbook@MacBook-Pro-2.local>
2022-03-15 14:43:06 -0300

    bueno

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,7 +181,8 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-var array = []
+var array = [0, 6, 12, 18, 24, 30, 36, 42, 48, 54, 60]
+{ return array}
 }
 
 function tieneTresDigitos(numero){
