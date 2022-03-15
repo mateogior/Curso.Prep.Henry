37b83ffca6a58cc5a3630d347a90ad0f99de4cbd
Mateo <macbook@MacBook-Pro-2.local>
2022-03-15 17:34:44 -0300

    aver

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -147,6 +147,11 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
+  if (numeroDeDia===0 || numeroDeDia===1){
+    return 'Es fin de semana'
+  } else {
+    return 'Es dia Laboral'
+  }
   
 } 
 
