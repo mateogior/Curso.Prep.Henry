c7abebccfc0a10255feb72abba78f664a43f808b
Mateo <macbook@MacBook-Pro-2.local>
2022-03-15 18:25:50 -0300

    aver

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -188,6 +188,12 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+var nuevoarray=[]
+  for (var i=0; i < array.length; i++){
+    if( array[i] === Enero && array[i] === Marzo && array[i]=== Noviembre){
+      return nuevoarray.push[i]
+    } else {return 'No se encontraron los meses pedidos'}
+  }
 }
 
 
