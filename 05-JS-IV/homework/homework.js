10bd0f07170517b75055394cbf4ad50ad34a840d
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 16:59:50 -0300

    aver;

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -85,6 +85,10 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(objeto[propiedad]= 'propiedad'){
+    return true
+  } else {return false;
+  }
 }
 
 function verificarPassword(usuario, password) {
