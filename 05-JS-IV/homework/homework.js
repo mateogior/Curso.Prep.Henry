dc5575b22cecf406b1c269f9c96b25e6abe054c6
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 15:18:58 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -22,6 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
+  property=null;
+  return property
 }
 
 function invocarMetodo(objeto, metodo) {
