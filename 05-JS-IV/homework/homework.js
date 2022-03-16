3864400739e2de0431b65a701295c1431a01a204
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 14:28:25 -0300

    bueno

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var gato={
+    nombre: nombre,
+    edad: edad,
+    saludar: function(){return 'Meow!'}
+    
+
+
+} ; return gato
 }
 
 
