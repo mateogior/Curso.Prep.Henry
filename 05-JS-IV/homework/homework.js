98d618dd7e68623be35dd8e2f6ff2e11684610b2
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 16:01:24 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -50,12 +50,15 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+  delete objeto[unaPropiedad];
+  return objeto
 }
 
 function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  
 
 }
 
