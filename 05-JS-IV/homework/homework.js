242a3ab041ec2fe3e8ca2acef16e855f8dec5361
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 15:57:04 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -33,13 +33,15 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código: 
-  objeto.metodo()
+  objeto[metodo]();
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  objetoMisterioso.numeroMisterioso * 5;
+  return numeroMisterioso
 
 }
 
