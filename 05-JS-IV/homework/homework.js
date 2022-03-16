a7eb06db544d718d215e3a1df9a264c06a380a2b
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 16:04:12 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -58,6 +58,11 @@ function nuevoUsuario(nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
+  var objeto={nombre: nombre,
+    email: email,
+    password: password,
+} ;
+return objeto 
   
 
 }
