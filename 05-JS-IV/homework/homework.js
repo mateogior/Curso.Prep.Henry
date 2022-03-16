7c0b34a0379de07a4ff2eae54bd680a5d2390819
Mateo <macbook@MacBook-Pro-2.local>
2022-03-16 15:51:51 -0300

    averrr

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -32,7 +32,8 @@ function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
-  // Tu código:
+  // Tu código: 
+  objeto.metodo()
 }
 
 function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
