73ff793a52d73aa4cbd868b39080de11e3c3152c
Mateo <macbook@MacBook-Pro-2.local>
2022-03-17 14:27:45 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -122,8 +122,9 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-  usuarios['usuario']['esPremium']=true;
-  return usuarios
+ for (var i = 0; i<usuarios.length; i++){
+   usuarios[i].esPremium=true
+ } return usuarios 
   
 }
 
