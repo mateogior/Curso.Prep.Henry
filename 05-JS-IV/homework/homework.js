c6abbdca2a720e125a5ec582ca629f2654098c3c
Mateo <macbook@MacBook-Pro-2.local>
2022-03-17 14:57:50 -0300

    aver

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -135,6 +135,11 @@ function sumarLikesDeUsuario(usuario) {
   // Suma todos los likes de todos los objetos "post"
   // Devuelve la suma
   // Tu código:
+  var suma=0
+  for (var i=0; i < usuario.posts.length; i++){
+    suma= suma+usuario.posts[i].likes
+  }
+  return suma 
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -147,6 +152,9 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  function calcularPrecioDescuento(){
+    producto.precio - (producto.precio * producto.porcentajeDeDescuento)
+  } return calcularPrecioDescuento 
 
 }
 
