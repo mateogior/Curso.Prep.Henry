1d5790284b492ab5e2c94c30625274e2ae321361
Mateo <macbook@MacBook-Pro-2.local>
2022-03-21 20:30:08 -0300

    aver

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -28,7 +28,7 @@ function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
-  Constructor.prototype.saludar();
+  Constructor.prototype.saludar= function()
   {return 'Hello World!'}
 }
 
