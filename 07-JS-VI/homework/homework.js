7f4b4dcbabbf4ed5ece6e3099c08b34f4ac1978d
Mateo <macbook@MacBook-Pro-2.local>
2022-03-22 17:16:28 -0300

    aver

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -47,6 +47,11 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoarray= array.map(function(elemento){
+    return cb(elemento);
+
+  });return nuevoarray
+
 }
 
 function filter(array) {
