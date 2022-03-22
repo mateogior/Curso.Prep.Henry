368acebd8c4b931418cd237d7743ea0ebcea17e1
Mateo <macbook@MacBook-Pro-2.local>
2022-03-22 11:54:04 -0300

    aver

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -43,8 +43,8 @@ function agregarStringInvertida() {
     var stringInvertida='';
     for(var i=this.length -1; i>= 0 ; i-- ){
       stringInvertida = stringInvertida + this.charAt(i);
-    } return stringInvertida 
-  }
+    } return stringInvertida ;
+  };
 }
 
 // ---------------------------------------------------------------------------//
