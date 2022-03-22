1007ffa3d7b432644b0b93236fe6ab5c182c2965
Mateo <macbook@MacBook-Pro-2.local>
2022-03-22 13:09:55 -0300

    aver

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,10 +4,10 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  var nuevonombre= nombre.map(function(nombre){
+  var nombre= nombre.map(function(nombre){
     return nombre.capitalize();
   });
-  return nuevonombre 
+ 
 }
 
 function invocarCallback(cb) {
