ea2ce55dbc7e7a14343cf7311e003d882c3dd9ff
Mateo <macbook@MacBook-Pro-2.local>
2022-03-25 16:40:37 -0300

    aver

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -75,6 +75,15 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+  for(var i=0; i<cadena.length; i++ ){
+    if(cadena[i]==='a' || cadena[i]==='b'|| cadena[i]==='c'){
+      cadena.pop(i)
+    } else {
+      return cadena 
+    }
+    
+  }
 }
 
 
