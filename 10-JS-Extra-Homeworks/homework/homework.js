66c86d209aa14528f26070a776c6ea63f8b630df
Mateo <macbook@MacBook-Pro-2.local>
2022-03-25 15:54:27 -0300

    aver

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -50,6 +50,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var strToArray= str.split(' ')
+  var arrayfinal= srToArray.map(function(ele){
+    return ele.split('').reverse().join('');
+  })
+   return arrayfinal.join(' ');
 } 
 
 
