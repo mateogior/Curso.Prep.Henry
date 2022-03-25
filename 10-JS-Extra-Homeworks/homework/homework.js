5c5cd58691be3014124e1104b6198342d4bbc527
Mateo <macbook@MacBook-Pro-2.local>
2022-03-25 18:04:20 -0300

    aver

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -93,14 +93,18 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-  var nuevoarr=[];
-  for (var i = 0; i<arr.length ; i++){
-    if (arr[i].length > arr[i+1].length){
-      nuevoarr= nuevoarr + arr[i] } else {
-        continue
-      } 
-
-  } return nuevoarr.split(' ')
+  var condicion=true;
+  while(condicion){
+    condicion=false;
+    for (var i = 0; i<arr.length - 1; i++){
+      if (arr[i].length > arr[i+1].length){
+        var aux=arr[i];
+         arr[i]= arr[i+1];
+         arr[i+1]= aux;
+         condicion=true;}
+      }
+  } return arr 
+  
 }
 
 
