adc00a37899bd080324b72a4bb7aaf4ac35fb6d5
Mateo <macbook@MacBook-Pro-2.local>
2022-03-25 16:25:02 -0300

    aver

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -63,10 +63,11 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
-  var numToString= numero.ToString();
-  if (numToString.split('')= numToSring.split('').reverse()) {
-    return 'Es capicua'
-  } else { return 'No es capicua'}
+  var numToString = numero.ToString();
+  if (numToString.split('')= numToString.split('').reverse()) {
+    return 'Es capicua';
+  } else { return 'No es capicua'
+}
 }
 
 
