e12dea0b689d2fa57b1f6272e0bbc4d2a19d015d
Mateo <macbook@MacBook-Pro-2.local>
2022-03-25 18:40:12 -0300

    aver

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -114,7 +114,7 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
-  nuevoarray=[];
+  var nuevoarray=[];
   for(var i= 0; i<arreglo1.length; i ++){
     for (var j=0; j<arreglo2.length; j++){
       if (arreglo1[i]===arreglo2[i]) {
